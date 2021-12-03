8d414a1db1aca274d77e31e682d1b8aa3180893a
Bart Reardon <bart.reardon@csiro.au>
2021-12-03 11:03:31 +1100

    Initial Commit

---
+++ imagetest/imagetestApp.swift
@@ -0,0 +1,17 @@
+//
+//  imagetestApp.swift
+//  imagetest
+//
+//  Created by Bart Reardon on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct imagetestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
